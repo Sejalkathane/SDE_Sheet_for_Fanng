17a3130b6c9d5976e3f03f039d111a4bab036d35
Sejal kathane <kathanesejal@gmail.com>
2023-09-28 08:54:04 +0530

    28-9 new negin

---
+++ NEW LEVEL UP/EASY/ARR_STR/14_Longest_Common_Prefix.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string longestCommonPrefix(vector<string> &str)
+{
+    int n = str.size();
+    if (n == 0)
+        return "";
+
+    string ans = "";
+
+    sort(str.begin(), str.end());
+
+    string a = str[0];
+    string b = str[n - 1];
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        if (a[i] == b[i])
+        {
+            ans = ans + a[i];
+        }
+        else
+        {
+            break;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+
+    vector<string>arr={"flower","flow","flight"};
+    cout<<longestCommonPrefix(arr);
+
+    return 0;
+}
